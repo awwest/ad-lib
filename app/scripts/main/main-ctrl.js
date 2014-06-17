5948d6849828e9f4e10a21dc910a52b2e6f1c81f
Davis Kim <dchk90@gmail.com>
2014-06-16 17:35:51 -0700

    Initial commit

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('famousAngularStarter')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $famous) {
+    var Transitionable = $famous['famous/transitions/Transitionable'];
     $scope.greeting = 'Hello, Famo.us';
+    $scope.number = 5;
+    $scope.offset = 50;
+    $scope.pictures = [];
+
+    $scope.$watch('number', function(num) {
+      var arr = [];
+      for(var i = 0; i < $scope.number; i++) {
+        var transitionable = new Transitionable([450*i + 50, $scope.offset, 1]);
+        arr.push({
+          translate: transitionable,
+          index: i
+        });
+      }
+      $scope.pictures = arr;
+    });
   });
