ab4df920331b62800de0f3ea00e2bd33b2930a41
Alex West <alexanderwwest@gmail.com>
2014-06-16 20:04:17 -0700

    Implements Image Surfaces

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -3,10 +3,17 @@
 angular.module('famousAngularStarter')
   .controller('MainCtrl', function ($scope, $famous) {
     var Transitionable = $famous['famous/transitions/Transitionable'];
+    var SlideData = require(['../images/SlideData']);
+    var Utility = require('famous/utilities/Utility');
     $scope.greeting = 'Hello, Famo.us';
     $scope.number = 5;
     $scope.offset = 50;
     $scope.pictures = [];
+    // var utility = new Utility();
+
+    // Utility.loadURL(SlideData.getUrl(), function(data){
+    //   $rootScope.data = data;
+    // });
 
     $scope.$watch('number', function(num) {
       var arr = [];
@@ -14,6 +21,7 @@ angular.module('famousAngularStarter')
         var transitionable = new Transitionable([450*i + 50, $scope.offset, 1]);
         arr.push({
           translate: transitionable,
+          photo: '../images/yeoman.png',
           index: i
         });
       }
