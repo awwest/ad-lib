4ac056615028d6dc586cc5eff1d975924aee943c
Davis Kim <dchk90@gmail.com>
2014-06-16 21:44:07 -0700

    Add draggability (buggy)

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -3,28 +3,57 @@
 angular.module('famousAngularStarter')
   .controller('MainCtrl', function ($scope, $famous) {
     var Transitionable = $famous['famous/transitions/Transitionable'];
-    var SlideData = require(['../images/SlideData']);
-    var Utility = require('famous/utilities/Utility');
+    var GenericSync    = $famous['famous/inputs/GenericSync'];
+    var MouseSync      = $famous['famous/inputs/MouseSync'];
+    var TouchSync      = $famous['famous/inputs/TouchSync'];
+    var EventHandler   = $famous['famous/core/EventHandler'];
+    var PhysicsEngine  = require('famous/physics/PhysicsEngine'); // not currently being used
+    var SlideData      = require(['../images/SlideData']); // not currently being used
+
+    // Set our sync to listen to mouse and touch events
+    GenericSync.register({
+      'mouse': MouseSync,
+      'touch': TouchSync
+    });
+
     $scope.greeting = 'Hello, Famo.us';
-    $scope.number = 5;
-    $scope.offset = 50;
-    $scope.pictures = [];
-    // var utility = new Utility();
+    $scope.numberOfPictures   = 3; // number of pictures
+    $scope.offset   = 50; // Y offset from top for where pictures start
+    $scope.pictures = []; // array of pictures
 
-    // Utility.loadURL(SlideData.getUrl(), function(data){
-    //   $rootScope.data = data;
-    // });
+    // var PE = new PhysicsEngine();
 
-    $scope.$watch('number', function(num) {
-      var arr = [];
-      for(var i = 0; i < $scope.number; i++) {
-        var transitionable = new Transitionable([450*i + 50, $scope.offset, 1]);
-        arr.push({
-          translate: transitionable,
+    for(var i = 0; i < $scope.numberOfPictures; i++) {
+      // keep each picture in its own closure scope using immediately invoked function
+      (function() {
+        var position = new Transitionable([450*i + 50, $scope.offset, 1]); // initial place
+
+        var pic = {
+          translate: position,
           photo: '../images/yeoman.png',
           index: i
+        };
+        pic.sync = new GenericSync(['mouse', 'touch'], function() { return position.get(); });
+
+        pic.EH = new EventHandler();
+        pic.EH.pipe(pic.sync);
+
+        pic.sync.on('update', function(data){
+          // console.log(data);
+          position.set(data.position); // TODO perhaps calculate position off delta?
         });
-      }
-      $scope.pictures = arr;
-    });
+
+        // pic.sync.on('end', function(data){
+        //   console.log(data);
+        //   //TODO: this is where we put in physics!
+        //   position.set([data.clientX, data.clientY], {
+        //     strength : 0.005,
+        //     velocity : data.velocity 
+        //   });
+        // });
+
+        $scope.pictures.push(pic);
+
+      })();
+    }
   });
